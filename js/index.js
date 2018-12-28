2ae506bcf86f7d594ce77f2dfab7d9015dd42e08
Adam777 <Adam777-5867@outlook.com>
2018-12-28 18:56:18 +0000

    Design updated and other Projects added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-$('body').scrollspy({ target: '.navbar-fixed-top' });
+const projectName = 'portfolio';
+localStorage.setItem('example_project', 'Personal Portfolio');
+
+$('body').scrollspy({
+	target: '#navbar'
+});
+
+$('a[href*="#"]').on('click', function(e) {
+	e.preventDefault();
+
+	$('html, body').animate({
+		scrollTop: $($(this).attr('href')).offset().top
+	}, 200, 'linear');
+});
