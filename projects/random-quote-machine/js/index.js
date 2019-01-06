9f5e349d0de9fe06d7ac2ee058476facbb11ea7c
Adam777 <Adam777-5867@outlook.com>
2019-01-06 16:35:55 +0000

    Fixes

--- projects/random-quote-machine/js/index.js
+++ projects/random-quote-machine/js/index.js
@@ -3,10 +3,10 @@
 
 $(document).ready(function() {
 	getQuote();
-});
 
-$('#new-quote').click(function() {
-	getQuote();
+	$('#new-quote').click(function() {
+		getQuote();
+	});
 });
 
 function getQuote() {
