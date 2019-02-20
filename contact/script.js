4e5e38b16fde18c6bd617b0f1861c1c71568cb15
Adam777 <Adam777-5867@outlook.com>
2019-02-20 11:26:12 +0000

    Update

--- contact/script.js
+++ contact/script.js
@@ -16,8 +16,8 @@ function onloadCallback() {
 
 function onSubmit(token) {
 	$('#submit-button').attr('disabled', 'disabled');
-	$('#contact-form-result .alert').alert('close');
-	$('#contact-form-result').html('<div class="alert alert-primary mt-2 mb-0" role="alert"><div class="spinner-border spinner-border-sm text-primary" role="status"><span class="sr-only">Loading...</span></div> <span>Sending...</span></div>');
+	$('#contact-form-message .alert').alert('close');
+	$('#contact-form-message').html('<div class="alert alert-primary mt-2 mb-0" role="alert"><div class="spinner-border spinner-border-sm text-primary" role="status"><span class="sr-only">Loading...</span></div> <span>Sending...</span></div>');
 
 	$.ajax({
 		url: 'https://usebasin.com/f/f8a55f3aacfc.json',
@@ -32,12 +32,12 @@ function onSubmit(token) {
 	})
 	.done(function() {
 		$('#submit-button').removeAttr('disabled');
-		$('#contact-form-result').html('<div class="alert alert-success alert-dismissible fade show mt-2 mb-0" role="alert"><span>Email sent successfully.</span><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
+		$('#contact-form-message').html('<div class="alert alert-success alert-dismissible fade show mt-2 mb-0" role="alert"><span>Email sent successfully.</span><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
 		// $('#name, #email, #message').val('');
 		$('#contact-form').trigger('reset');
 	})
 	.fail(function() {
 		$('#submit-button').removeAttr('disabled');
-		$('#contact-form-result').html('<div class="alert alert-danger alert-dismissible fade show mt-2 mb-0" role="alert"><span>Error. Please try again.</span><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
+		$('#contact-form-message').html('<div class="alert alert-danger alert-dismissible fade show mt-2 mb-0" role="alert"><span>Error. Please try again.</span><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
 	});
 }
