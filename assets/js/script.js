ef6fe6578e6f0589e299986855b543fa9a719457
Adam777 <Adam777-5867@outlook.com>
2021-09-19 18:25:14 +0000

    Bootstrap updated to v5.1.1 and other changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
-$(document).ready(function() {
-	// $('body').scrollspy({
-	// 	target: '#navbar'
-	// });
+// $(document).ready(function() {
+// 	$('body').scrollspy({
+// 		target: '#navbar'
+// 	});
 
-	// Smooth Scroll to ID
-	// $('a[href*="#"]').on('click', function(e) {
-	// 	e.preventDefault();
+// 	// Smooth Scroll to ID
+// 	$('a[href*="#"]').on('click', function(e) {
+// 		e.preventDefault();
 
-	// 	$('html, body').animate({
-	// 		scrollTop: $($(this).attr('href')).offset().top
-	// 	}, 200, 'linear');
-	// });
-});
+// 		$('html, body').animate({
+// 			scrollTop: $($(this).attr('href')).offset().top
+// 		}, 200, 'linear');
+// 	});
+// });
