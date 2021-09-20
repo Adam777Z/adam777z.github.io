0ff6f4f1111cf308ea88cb106d01aba4f41d3df9
Adam777 <Adam777-5867@outlook.com>
2021-09-20 12:51:35 +0000

    Cleanup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,2 @@
 document.addEventListener('DOMContentLoaded', function (event) {
-	new bootstrap.ScrollSpy(document.body, {
-		target: '#navbar'
-	});
-
-	// Smooth Scroll to ID
-	document.querySelector('a[href^="#"]').addEventListener('click', function (event) {
-		event.preventDefault();
-		document.querySelector(this.getAttribute('href')).scrollIntoView({ 'behavior': 'smooth' });
-	});
 });
