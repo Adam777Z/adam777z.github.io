43f70d0ca1a7a9b9fe3ea4fa586c5a8976f6b8a9
Adam777 <Adam777-5867@outlook.com>
2021-09-20 12:48:24 +0000

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function (event) {
-	// new bootstrap.ScrollSpy(document.body, {
-	// 	target: '#navbar'
-	// });
+	new bootstrap.ScrollSpy(document.body, {
+		target: '#navbar'
+	});
 
 	// Smooth Scroll to ID
 	document.querySelector('a[href^="#"]').addEventListener('click', function (event) {
